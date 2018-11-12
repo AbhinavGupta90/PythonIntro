1a11407aa162c08c6fb5d1b501b2110cb3c1ea71
bwielk <bwielk@gmail.com>
2018-11-12 22:58:16 +0000

    Some more pythony actions

---
+++ venv/data_types.py
@@ -0,0 +1,34 @@
+integer_value = 234456
+float_value = 3.456
+
+bool_value = True
+bool_value_false = False
+
+string_value = 'string'
+a_single_character = 'a'
+
+print(type(integer_value))
+print(type(float_value))
+print(type(bool_value))
+print(type(bool_value_false))
+print(type(string_value))
+print(type(a_single_character))
+
+#division in python
+print("Division in Python")
+int1 = 20
+int2 = 10
+print(type(int2/int1))
+print(type(int1/int2))
+
+print("Slash-slash")
+print(type(int2//int1))
+print(type(int1//int2))
+
+print(int2**int1)
+print(pow(int2, int1))
+print(round(2.33442, 3))
+print(round(2.33452, 3))
+print(round(2.33442, 4))
+print(max(23, 11, 1, 23.1))
+print(min(2, 344, 0.1))
--- venv/multiplication_table.py
+++ venv/multiplication_table.py
@@ -1,7 +1,18 @@
 from fstring import fstring
 
-def print_multiplication_table(table, start, end):
+def print_multiplication_table(table, start=1, end=10):
+    print("NEW EXECUTION")
     for i in range(start, end+1):
         print (fstring("{table} X {i} = {table*i}"))
 
-print_multiplication_table(2, 1, 11)
+# print_multiplication_table(2, 1, 11)
+# print_multiplication_table(12, 1, 13)
+# print_multiplication_table(10)
+
+def multiply_up_to_three_values(val1=1, val2=1, val3=1):
+    result = val1*val2*val3
+    return result
+
+print(multiply_up_to_three_values(2,3)) #should equal 6
+print(multiply_up_to_three_values(1,4,9)) #should equal 36
+
