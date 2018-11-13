c62180385068ab980704c03ab994b0d7b3288d69
bwielk <bwielk@gmail.com>
2018-11-13 22:47:20 +0000

    More playing around with python

--- venv/data_types.py
+++ venv/data_types.py
@@ -32,3 +32,22 @@ print(round(2.33452, 3))
 print(round(2.33442, 4))
 print(max(23, 11, 1, 23.1))
 print(min(2, 344, 0.1))
+
+##Strings
+
+message = "Hello! Welcome to Python!"
+lowercase_string = "i am lowercase"
+print(message.upper())
+print(message.lower())
+print(lowercase_string.capitalize())
+print(lowercase_string.find("am")) ##2
+print(lowercase_string.casefold())
+print("Es ist suß".casefold()) ## ß should be converted to ss
+print("Ich wohne in Friedrichstraße".casefold()) ## ß should be converted#  to ss
+print(message.isnumeric()) #false
+print(message.istitle()) #false
+print("Hello! Welcome To Python".istitle()) #true
+print("1234abs".isnumeric()) #false
+print("1234abs".isalpha()) #false
+print("122333445".isnumeric()) #true
+print("MEINNAMENISTBLAZEJ".isalpha()) #true
