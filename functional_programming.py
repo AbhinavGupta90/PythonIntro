c27e0bb4305678591ae8ab176c0d699af8d31c53
bwielk <bwielk@gmail.com>
2018-11-25 02:13:00 +0000

    More lambdas

--- functional_programming.py
+++ functional_programming.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def multiply_by_two(number):
     return number*2
 
@@ -44,7 +46,7 @@ def words_beginning_with_a_character(char):
         error_message = "You need to enter a single character for the function. You have entered '%s'" % char
         raise ValueError(error_message)
     else:
-        list_of_results = list(filter(lambda word: word.lower().startswith(char), words))
+        list_of_results = list(filter(lambda word: word.lower().startswith(char.lower()), words))
         if len(list_of_results) == 0:
             return "No results found"
         else:
@@ -52,8 +54,33 @@ def words_beginning_with_a_character(char):
 
 print(words_beginning_with_a_character("a"))
 print(words_beginning_with_a_character("z"))
-print(words_beginning_with_a_character("aaz"))
-print(words_beginning_with_a_character(4))
+#print(words_beginning_with_a_character("aaz"))
+#print(words_beginning_with_a_character(4))
+print(words_beginning_with_a_character("C"))
 
+list_of_uppercased_words = map(lambda word: word.upper(), words)
+print(list(list_of_uppercased_words))
 
+list_of_doubled_words = map(lambda word: word + word, words)
+print(list(list_of_doubled_words))
 
+list_of_reversed_words = map(lambda word: word[::-1], words)
+print(list(list_of_reversed_words))
+
+list_of_first_ten_numbers_squared = map(lambda num: num**2, list_of_numbers[0:10])
+print(list(list_of_first_ten_numbers_squared))
+
+list_of_squared_numbers_using_range = map(lambda num: num*num, range(100, 140))
+print(list(list_of_squared_numbers_using_range))
+
+reduce_example = reduce(lambda x,y: x+y, list_of_numbers)
+print(reduce_example)
+
+one_long_string = reduce(lambda word1, word2: word1+word2, words).lower()
+print(one_long_string)
+
+maximum_length_words = reduce(lambda x,y: x if len(x) > len(y) else y, words)
+print(maximum_length_words)
+
+minimum_length_words = reduce(lambda x,y: x if len(x) < len(y) else y, words)
+print(minimum_length_words)
