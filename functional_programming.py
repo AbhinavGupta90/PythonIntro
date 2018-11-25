2603d25763f85c4fe1f99ae995da1606e33dcf5d
bwielk <bwielk@gmail.com>
2018-11-25 01:32:32 +0000

    more on lambdas

--- functional_programming.py
+++ functional_programming.py
@@ -32,8 +32,28 @@ print(filtered_list)
 filter(lambda num: print(num%2), list_of_numbers)
 
 words = ["Apple", "Bat", "Juice", "fainT", "Ape", "Bolt", "Brother", "Taste", "parT", "crawL"]
+
 words_ending_with_t = list(filter(lambda word: word.lower().endswith("t"), words))
 print(words_ending_with_t)
+
+words_with_length_of_greater_than_3 = list(filter(lambda word: len(word) > 3, words))
+print(words_with_length_of_greater_than_3)
+
+def words_beginning_with_a_character(char):
+    if len(char) > 1 or type(char) != str:
+        error_message = "You need to enter a single character for the function. You have entered '%s'" % char
+        raise ValueError(error_message)
+    else:
+        list_of_results = list(filter(lambda word: word.lower().startswith(char), words))
+        if len(list_of_results) == 0:
+            return "No results found"
+        else:
+            return list_of_results
+
+print(words_beginning_with_a_character("a"))
+print(words_beginning_with_a_character("z"))
+print(words_beginning_with_a_character("aaz"))
+print(words_beginning_with_a_character(4))
 
 
 
