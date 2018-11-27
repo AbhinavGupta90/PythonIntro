a34d2f6b50972040c9c328f5cc2896ec6c639a9c
bwielk <bwielk@gmail.com>
2018-11-27 22:17:36 +0000

    Playing around with arguments

--- some_tips.py
+++ some_tips.py
@@ -31,7 +31,23 @@ print(Currency(2))
 #METHODS AND ARGS
 
 def example_method(mandatory, default="Default", *args, **kwargs):
-    print("\n\n MANDATORY => %s\n DEFAULT => %s\n ARGS => %s\n KWARGS => %s" % (mandatory, default, args, kwargs))
+    print("\n\n MANDATORY => %s type{%s}\n DEFAULT => %s type{%s}\n ARGS => %s type{%s}\n KWARGS => %s type{%s}"
+          % (mandatory, type(mandatory), default, type(default), args, type(args), kwargs, type(kwargs)))
 
 example_method(15)
-example_method(14,28)
+## MANDATORY => 15 DEFAULT => Default ARGS => () KWARGS => {}
+
+example_method(14,28, "el1", "el2", "el4", "el3")
+## MANDATORY => 14 DEFAULT => 28 ARGS => ('el1', 'el2', 'el4', 'el3') KWARGS => {}
+
+example_method(120, "string1", "string2", "string3", "string4", name="Blazej", surname="Wielk", eng_equiv_name="Blaise")
+## MANDATORY => 120 type{<class 'int'>}
+## DEFAULT => string1 type{<class 'str'>}
+## ARGS => ('string2', 'string3', 'string4') type{<class 'tuple'>}
+## KWARGS => {'name': 'Blazej', 'eng_equiv_name': 'Blaise', 'surname': 'Wielk'} type{<class 'dict'>}
+
+example_method(name="John", surname="McT", initials="AJM", mandatory=29, default="Twenty")
+## MANDATORY => 29 type{<class 'int'>}
+## DEFAULT => Twenty type{<class 'str'>}
+## ARGS => () type{<class 'tuple'>}
+## KWARGS => {'name': 'John', 'surname': 'McT', 'initials': 'AJM'} type{<class 'dict'>}
